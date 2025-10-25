152a260aef71d569acd75457c7a07009be1e0d0f
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-25 16:17:18 +0300

    add register and login controller functions

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZwalksAPI.Models.DTO;
+
+namespace NZwalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager )
+        {
+            this.userManager = userManager;
+        }
+        //register method route : api/auth/register
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+        {
+
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDTO.UserName,
+                Email = registerRequestDTO.UserName
+            };
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDTO.Password);
+
+            if(identityResult.Succeeded)
+            {
+                //add roles to user
+                if(registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
+
+                    if(identityResult.Succeeded)
+                    {
+                        return Ok("register was successfull");
+                    }
+                }
+            }
+
+            return BadRequest("something went wrong");
+
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(LoginRequestDTO loginRequestDTO)
+        {
+            var user = await userManager.FindByEmailAsync(loginRequestDTO.UserName);
+
+            if(user != null)
+            {
+                var checkPassword = await userManager.CheckPasswordAsync(user,loginRequestDTO.Password);
+
+                if (checkPassword)
+                {
+                    return Ok(" good job !")
+                }
+            }
+
+            return BadRequest("something was wrong")
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -35,7 +35,20 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
 builder.Services.AddIdentityCore<IdentityUser>().AddRoles<IdentityRole>()
     .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")
-    .AddEntityFrameworkStores<NZWalksAtuhDbContext>();
+    .AddEntityFrameworkStores<NZWalksAtuhDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredUniqueChars = 1;
+
+
+});
 
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
