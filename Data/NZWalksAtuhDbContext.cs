e912dbff5f2bbe7d6dd586598c21c13841b7f116
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-25 14:52:58 +0300

    made new roles

--- Data/NZWalksAtuhDbContext.cs
+++ Data/NZWalksAtuhDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZwalksAPI.Data
@@ -7,6 +8,38 @@ namespace NZwalksAPI.Data
     {
         public NZWalksAtuhDbContext(DbContextOptions<NZWalksAtuhDbContext> options) : base(options)
         {
+
+          
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var ReadRoleId = "5f8e170e-8186-4f54-8cbf-ca56503075fa";
+            var WriterRoleId = "41f7fc5e-0d33-4c40-8c31-04ada5b39933";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = ReadRoleId,
+                    ConcurrencyStamp = ReadRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader"
+
+                },
+                new IdentityRole
+                {
+                    Id = WriterRoleId,
+                    ConcurrencyStamp= WriterRoleId,
+                    Name = "Writer",
+                    NormalizedName ="Writer".ToUpper()
+
+                }
+
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
